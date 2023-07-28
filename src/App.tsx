b7558b0a1bd48789c507f2038c51c86a37863523
quilantera <gugaquilante@gmail.com>
2023-07-28 10:52:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import './App.css';
+import { domingo, sabado } from './integrantes';
+
+
+
+function App() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [day, setDay] = useState('sabado');
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prev) => !prev);
+    document.body.classList.toggle('dark-mode');
+  };
+  const toggleTable = (change: string) => {
+    setDay(()=> change)
+  }
+  function showTable(){
+  const dia = (day == 'sabado') ? sabado:domingo;
+       return(<table>
+          <thead>
+            <tr >
+              <th>Horário</th>
+            {dia[0].map((evento, index) => (
+                  <th key={"titulo"+index}>{evento.local}</th>
+              ))
+            } 
+            </tr>
+          </thead>
+          <tbody>
+            {
+              dia.map((horario,index)=>(
+                <tr>                
+                <td  className='horario'>{7+index*2+':00'}</td>
+                  {horario.map((evento)=>(
+                    <td>  {evento.pessoa1}
+                    {evento.pessoa2 && ` e ${evento.pessoa2}`}
+                    {evento.pessoa3 && ` e ${evento.pessoa3}`}</td>
+                  ))}
+                </tr>
+              ))
+            }
+          
+          </tbody>
+        </table>);
+  }
+  return (
+    <main> 
+       <button className={isDarkMode ? 'dark-mode' : ''} onClick={toggleDarkMode}>
+        {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+      </button>
+      <h1>Programação Transito EJC 2023</h1>
+      <div className='button-area'>
+      <button className={day ==='sabado' ? 'dia selected' : 'dia'} onClick={()=>toggleTable('sabado')}>
+        <h2>Tabela Sábado</h2>
+      </button>
+      <button className={day ==='domingo' ? 'dia selected' : 'dia'} onClick={()=>toggleTable('domingo')}>
+        <h2>Domingo</h2>
+      </button>
+      </div>
+      <div>
+      
+        {showTable() } 
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/integrantes.ts
@@ -0,0 +1,716 @@
+export const nomes = [
+    'Daniel',
+    'Felipe',
+    'Henrique',
+    'Italo',
+    'Marcelo',
+    'Marianna',
+    'Marlon',
+    'Matheus N',
+    'Milena',
+    'Murilo',
+    'Raissa',
+    'Samuel',
+    'Victoria',
+    'Lucas',
+    'Tia Camila M',
+    'Tia Stephanie',
+    'Tio Fabio',
+    'Tia Camila P',
+    'Tio Thiago',
+    'Tia Ariane',
+    'Tio Matheus Y',
+]
+export const sabado =[
+[
+    {
+        local:'Plenário',
+        pessoa1:'Murilo',
+        pessoa2:'Victoria',
+    },
+    {
+        local:'Escada Plenário',
+        pessoa1:'Daniel',
+        pessoa2:'Raissa',
+    },
+    {
+        local:'Circulo',
+        pessoa1:'Italo',
+        pessoa2:'Marcelo',
+    },
+    {
+        local:'Portão Jovens',
+        pessoa1:'Felipe',
+        pessoa2:'Tia Camila M',
+    },
+    {
+        local:'Escada Capela',
+        pessoa1:'Matheus N',
+        pessoa2:'Marianna',
+    },
+    {
+        local:'Capela/Liturgia',
+        pessoa1:'Samuel',
+        pessoa2:'Milena',
+    },
+    {
+        local:'Escada Equipes',
+        pessoa1:'Lucas',
+        pessoa2:'Henrique',
+    },
+    {
+        local:'Portão Equipes',
+        pessoa1:'Tia Stephanie',
+        pessoa2:'Tia Camila P',
+    },
+    {
+        local:'Estacionamento',
+        pessoa1:'Tio Fabio',
+        pessoa2:'Tio Thiago',
+        pessoa3:'Marlon'
+    },
+    {
+        local:'Descanso',
+        pessoa1:'Tia Ariane',
+        pessoa2:'Tio Matheus Y',
+    }
+],
+[
+    {
+        local:'Plenário',
+        pessoa1:'Daniel',
+        pessoa2:'Raissa',
+    },
+    {
+        local:'Escada Plenário',
+        pessoa1:'Murilo',
+        pessoa2:'Victoria',
+    },
+    {
+        local:'Circulo',
+        pessoa1:'Lucas',
+        pessoa2:'Henrique',
+    },
+    {
+        local:'Portão Jovens',
+        pessoa1:'Italo',
+        pessoa2:'Marcelo',
+    },
+    {
+        local:'Escada Capela',
+        pessoa1:'Samuel',
+        pessoa2:'Milena',
+    },
+    {
+        local:'Capela/Liturgia',
+        pessoa1:'Matheus N',
+        pessoa2:'Marianna',
+    },
+    {
+        local:'Escada Equipes',
+        pessoa1:'Tio Fabio',
+        pessoa2:'Marlon',
+    },
+    {
+        local:'Portão Equipes',
+        pessoa1:'Tia Ariane',
+        pessoa2:'Tio Matheus Y',
+    },
+    {
+        local:'Estacionamento',
+        pessoa1:'Felipe',
+        pessoa2:'Tia Camila M',
+        pessoa3:'Tio Thiago'
+    },
+    {
+        local:'Descanso',
+        pessoa1:'Tia Stephanie',
+        pessoa2:'Tia Camila P',
+    }
+],
+[
+    {
+        local:'Plenário',
+        pessoa1:'Matheus N',
+        pessoa2:'Marianna',
+    },
+    {
+        local:'Escada Plenário',
+        pessoa1:'Samuel',
+        pessoa2:'Milena',
+    },
+    {
+        local:'Circulo',
+        pessoa1:'Murilo',
+        pessoa2:'Victoria',
+    },
+    {
+        local:'Portão Jovens',
+        pessoa1:'Felipe',
+        pessoa2:'Tia Camila M',
+    },
+    {
+        local:'Escada Capela',
+        pessoa1:'Daniel',
+        pessoa2:'Raissa',
+    },
+    {
+        local:'Capela/Liturgia',
+        pessoa1:'Tia Camila P',
+        pessoa2:'Marlon',
+    },
+    {
+        local:'Escada Equipes',
+        pessoa1:'Tia Ariane',
+        pessoa2:'Tio Matheus Y',
+    },
+    {
+        local:'Portão Equipes',
+        pessoa1:'Italo',
+        pessoa2:'Marcelo',
+    },
+    {
+        local:'Estacionamento',
+        pessoa1:'Tio Fabio',
+        pessoa2:'Tio Stephanie',
+        pessoa3:'Henrique'
+    },
+    {
+        local:'Descanso',
+        pessoa1:'Tio Thiago',
+        pessoa2:'Lucas',
+    }
+],
+[
+    {
+        local:'Plenário',
+        pessoa1:'Samuel',
+        pessoa2:'Milena',
+    },
+    {
+        local:'Escada Plenário',
+        pessoa1:'Matheus N',
+        pessoa2:'Marianna',
+    },
+    {
+        local:'Circulo',
+        pessoa1:'Tia Camila P',
+        pessoa2:'Marlon',
+    },
+    {
+        local:'Portão Jovens',
+        pessoa1:'Tia Stephanie',
+        pessoa2:'Tio Fabio',
+    },
+    {
+        local:'Escada Capela',
+        pessoa1:'Murilo',
+        pessoa2:'Victoria',
+    },
+    {
+        local:'Capela/Liturgia',
+        pessoa1:'Daniel',
+        pessoa2:'Raissa',
+    },
+    {
+        local:'Escada Equipes',
+        pessoa1:'Italo',
+        pessoa2:'Marcelo',
+    },
+    {
+        local:'Portão Equipes',
+        pessoa1:'Tia Ariane',
+        pessoa2:'Tio Matheus',
+    },
+    {
+        local:'Estacionamento',
+        pessoa1:'Tio Thiago',
+        pessoa2:'Lucas',
+        pessoa3:'Felipe'
+    },
+    {
+        local:'Descanso',
+        pessoa1:'Henrique',
+        pessoa2:'Tia Camila M',
+    }
+],
+[
+    {
+        local:'Plenário',
+        pessoa1:'Tia Ariane',
+        pessoa2:'Tio Matheus Y',
+    },
+    {
+        local:'Escada Plenário',
+        pessoa1:'Tio Thiago',
+        pessoa2:'Tia Stephanie',
+    },
+    {
+        local:'Circulo',
+        pessoa1:'Matheus M',
+        pessoa2:'Marianna',
+    },
+    {
+        local:'Portão Jovens',
+        pessoa1:'Daniel',
+        pessoa2:'Raissa',
+    },
+    {
+        local:'Escada Capela',
+        pessoa1:'Lucas',
+        pessoa2:'Henrique',
+    },
+    {
+        local:'Capela/Liturgia',
+        pessoa1:'Murilo',
+        pessoa2:'Victoria',
+    },
+    {
+        local:'Escada Equipes',
+        pessoa1:'Tia Camila P',
+        pessoa2:'Marlon',
+    },
+    {
+        local:'Portão Equipes',
+        pessoa1:'Samuel',
+        pessoa2:'Milena',
+    },
+    {
+        local:'Estacionamento',
+        pessoa1:'Tio Fabio',
+        pessoa2:'Italo',
+        pessoa3:'Tia Camila M'
+    },
+    {
+        local:'Descanso',
+        pessoa1:'Felipe',
+        pessoa2:'Marcelo',
+    }
+],
+[
+    {
+        local:'Plenário',
+        pessoa1:'Daniel',
+        pessoa2:'Raissa',
+    },
+    {
+        local:'Escada Plenário',
+        pessoa1:'Tia Ariane',
+        pessoa2:'Tio Matheus Y',
+    },
+    {
+        local:'Circulo',
+        pessoa1:'Italo',
+        pessoa2:'Marcelo',
+    },
+    {
+        local:'Portão Jovens',
+        pessoa1:'Samuel',
+        pessoa2:'Milena',
+    },
+    {
+        local:'Escada Capela',
+        pessoa1:'Tia Stephanie',
+        pessoa2:'Tio Fabio',
+    },
+    {
+        local:'Capela/Liturgia',
+        pessoa1:'Murilo',
+        pessoa2:'Victoria',
+    },
+    {
+        local:'Escada Equipes',
+        pessoa1:'Matheus N',
+        pessoa2:'Marianna',
+    },
+    {
+        local:'Portão Equipes',
+        pessoa1:'Lucas',
+        pessoa2:'Henrique',
+    },
+    {
+        local:'Estacionamento',
+        pessoa1:'Tia Camila P',
+        pessoa2:'Tio Thiago',
+        pessoa3:'Marcelo'
+    },
+    {
+        local:'Descanso',
+        pessoa1:'Italo',
+        pessoa2:'Marlon',
+    }
+],
+[
+    {
+        local:'Plenário',
+        pessoa1:'Murilo',
+        pessoa2:'Victoria',
+    },
+    {
+        local:'Escada Plenário',
+        pessoa1:'Lucas',
+        pessoa2:'Henrique',
+    },
+    {
+        local:'Circulo',
+        pessoa1:'Daniel',
+        pessoa2:'Raissa',
+    },
+    {
+        local:'Portão Jovens',
+        pessoa1:'Tia Ariane',
+        pessoa2:'Tio Matheus Y',
+    },
+    {
+        local:'Escada Capela',
+        pessoa1:'Italo',
+        pessoa2:'Marcelo',
+    },
+    {
+        local:'Capela/Liturgia',
+        pessoa1:'Samuel',
+        pessoa2:'Milena',
+    },
+    {
+        local:'Escada Equipes',
+        pessoa1:'Felipe',
+        pessoa2:'Tia Camila M',
+    },
+    {
+        local:'Portão Equipes',
+        pessoa1:'Tia Camila P',
+        pessoa2:'Marlon',
+    },
+    {
+        local:'Estacionamento',
+        pessoa1:'Tio Fabio',
+        pessoa2:'Tio Thiago',
+        pessoa3:'Tia Stephanie'
+    },
+    {
+        local:'Descanso',
+        pessoa1:'Matheus N',
+        pessoa2:'Marianna',
+    }
+],
+]
+export const domingo =[
+    [
+    {
+        local:'Plenário',
+        pessoa1:'Felipe',
+        pessoa2:'Tia Camila M',
+    },
+    {
+        local:'Escada Plenário',
+        pessoa1:'Matheus N',
+        pessoa2:'Marianna',
+    },
+    {
+        local:'Circulo',
+        pessoa1:'Daniel',
+        pessoa2:'Raissa',
+    },
+    {
+        local:'Portão Jovens',
+        pessoa1:'Italo',
+        pessoa2:'Marcelo',
+    },
+    {
+        local:'Escada Capela',
+        pessoa1:'Tia Camila P',
+        pessoa2:'Marlon',
+    },
+    {
+        local:'Capela/Liturgia',
+        pessoa1:'Lucas',
+        pessoa2:'Henrique',
+    },
+    {
+        local:'Escada Equipes',
+        pessoa1:'Tia Ariane',
+        pessoa2:'Tio Matheus Y',
+    },
+    {
+        local:'Portão Equipes',
+        pessoa1:'Tia Stephanie',
+        pessoa2:'Victoria',
+    },
+    {
+        local:'Estacionamento',
+        pessoa1:'Tio Fabio',
+        pessoa2:'Tio Thiago',
+        pessoa3:'Murilo'
+    },
+    {
+        local:'Descanso',
+        pessoa1:'Samuel',
+        pessoa2:'Milena',
+    }
+],
+[
+    {
+        local:'Plenário',
+        pessoa1:'Daniel',
+        pessoa2:'Raissa',
+    },
+    {
+        local:'Escada Plenário',
+        pessoa1:'Lucas',
+        pessoa2:'Henrique',
+    },
+    {
+        local:'Circulo',
+        pessoa1:'Matheus N',
+        pessoa2:'Marianna',
+    },
+    {
+        local:'Portão Jovens',
+        pessoa1:'Tia Stephanie',
+        pessoa2:'Tio Fabio',
+    },
+    {
+        local:'Escada Capela',
+        pessoa1:'Italo',
+        pessoa2:'Marcelo',
+    },
+    {
+        local:'Capela/Liturgia',
+        pessoa1:'Tia Ariane',
+        pessoa2:'Tio Matheus Y',
+    },
+    {
+        local:'Escada Equipes',
+        pessoa1:'Tia Camila P',
+        pessoa2:'Tio Marlon',
+    },
+    {
+        local:'Portão Equipes',
+        pessoa1:'Samuel',
+        pessoa2:'Milena',
+    },
+    {
+        local:'Estacionamento',
+        pessoa1:'Felipe',
+        pessoa2:'Tia Camila M',
+        pessoa3:'Tio Thiago'
+    },
+    {
+        local:'Descanso',
+        pessoa1:'Murilo',
+        pessoa2:'Victoria',
+    }
+],
+[
+    {
+        local:'Plenário',
+        pessoa1:'Murilo',
+        pessoa2:'Victoria',
+    },
+    {
+        local:'Escada Plenário',
+        pessoa1:'Tia Ariane',
+        pessoa2:'Tio Matheus Y',
+    },
+    {
+        local:'Circulo',
+        pessoa1:'Samuel',
+        pessoa2:'Milena',
+    },
+    {
+        local:'Portão Jovens',
+        pessoa1:'Tio Thiago',
+        pessoa2:'Marlon',
+    },
+    {
+        local:'Escada Capela',
+        pessoa1:'Matheus N',
+        pessoa2:'Marianna',
+    },
+    {
+        local:'Capela/Liturgia',
+        pessoa1:'Felipe',
+        pessoa2:'Tia Camila M',
+    },
+    {
+        local:'Escada Equipes',
+        pessoa1:'Italo',
+        pessoa2:'Marcelo',
+    },
+    {
+        local:'Portão Equipes',
+        pessoa1:'Lucas',
+        pessoa2:'Henrique',
+    },
+    {
+        local:'Estacionamento',
+        pessoa1:'Tia Stephanie',
+        pessoa2:'Tio Fabio',
+        pessoa3:'Tia Camila P'
+    },
+    {
+        local:'Descanso',
+        pessoa1:'Daniel',
+        pessoa2:'Raissa',
+    }
+],
+[
+    {
+        local:'Plenário',
+        pessoa1:'Samuel',
+        pessoa2:'Milena',
+    },
+    {
+        local:'Escada Plenário',
+        pessoa1:'Daniel',
+        pessoa2:'Raissa',
+    },
+    {
+        local:'Circulo',
+        pessoa1:'Murilo',
+        pessoa2:'Victoria',
+    },
+    {
+        local:'Portão Jovens',
+        pessoa1:'Tia Ariane',
+        pessoa2:'Tio Matheus Y',
+    },
+    {
+        local:'Escada Capela',
+        pessoa1:'Tia Stephanie',
+        pessoa2:'Tio Fabio',
+    },
+    {
+        local:'Capela/Liturgia',
+        pessoa1:'Matheus N',
+        pessoa2:'Marianna',
+    },
+    {
+        local:'Escada Equipes',
+        pessoa1:'Felipe',
+        pessoa2:'Tia Camila M',
+    },
+    {
+        local:'Portão Equipes',
+        pessoa1:'Italo',
+        pessoa2:'Marcelo',
+    },
+    {
+        local:'Estacionamento',
+        pessoa1:'Tio Fabio',
+        pessoa2:'Marlon',
+        pessoa3:'Henrique'
+    },
+    {
+        local:'Descanso',
+        pessoa1:'Tia Camila P',
+        pessoa2:'Lucas',
+    }
+],
+[
+    {
+        local:'Plenário',
+        pessoa1:'Matheus N',
+        pessoa2:'Marianna',
+    },
+    {
+        local:'Escada Plenário',
+        pessoa1:'Tia Stephanie',
+        pessoa2:'Tio Matheus Y',
+    },
+    {
+        local:'Circulo',
+        pessoa1:'Felipe',
+        pessoa2:'Tia Camila M',
+    },
+    {
+        local:'Portão Jovens',
+        pessoa1:'Samuel',
+        pessoa2:'Milena',
+    },
+    {
+        local:'Escada Capela',
+        pessoa1:'Daniel',
+        pessoa2:'Raissa',
+    },
+    {
+        local:'Capela/Liturgia',
+        pessoa1:'Italo',
+        pessoa2:'Marcelo',
+    },
+    {
+        local:'Escada Equipes',
+        pessoa1:'Murilo',
+        pessoa2:'Victoria',
+    },
+    {
+        local:'Portão Equipes',
+        pessoa1:'Matheus N',
+        pessoa2:'Marianna',
+    },
+    {
+        local:'Estacionamento',
+        pessoa1:'Tio Fabio',
+        pessoa2:'Lucas',
+        pessoa3:'Ariane'
+    },
+    {
+        local:'Descanso',
+        pessoa1:'Marlon',
+        pessoa2:'Henrique',
+    }
+],
+[
+    {
+        local:'Plenário',
+        pessoa1:'Lucas',
+        pessoa2:'Henrique',
+    },
+    {
+        local:'Escada Plenário',
+        pessoa1:'Felipe',
+        pessoa2:'Tia Camila M',
+    },
+    {
+        local:'Circulo',
+        pessoa1:'Tia Ariane',
+        pessoa2:'Tio Matheus Y',
+    },
+    {
+        local:'Portão Jovens',
+        pessoa1:'Italo',
+        pessoa2:'Marcelo',
+    },
+    {
+        local:'Escada Capela',
+        pessoa1:'Samuel',
+        pessoa2:'Milena',
+    },
+    {
+        local:'Capela/Liturgia',
+        pessoa1:'Tia Camila P',
+        pessoa2:'Marlon',
+    },
+    {
+        local:'Escada Equipes',
+        pessoa1:'Daniel',
+        pessoa2:'Raissa',
+    },
+    {
+        local:'Portão Equipes',
+        pessoa1:'Murilo',
+        pessoa2:'Victoria',
+    },
+    {
+        local:'Estacionamento',
+        pessoa1:'Tio Fabio',
+        pessoa2:'Tio Thiago',
+        pessoa3:'tia Stephanie'
+    },
+    {
+        local:'Descanso',
+        pessoa1:'Matheus N',
+        pessoa2:'Marinna',
+    }
+],
+]
